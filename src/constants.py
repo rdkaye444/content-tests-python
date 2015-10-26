edca426eaba672e6fb19296eece3d9cca332c470
Richard <richard.kaye@scala.com>
2015-10-26 08:31:23 -0400

    Set up project.  Create requirements.txt file.  outline source for test.  Set up config file and log file.

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,4 @@ from enum import Enum
 
 CONFIG_FILE_PATH ='./conf/conf.test'
 LOG_FILE_PATH = './log/test.log'
+LOG_CONFIG_FILE_PATH = './conf/log_config'
--- src/content-test.py
+++ src/content-test.py
@@ -6,8 +6,36 @@ from res.constants import *
 
 login_token = ""
 
-def setup():
+def log_response(response_data):
+	'''Log the response from any http request in one place, and in one format
+	'''
+	pass
+
+def login():
+	''' Log in and get login token'''
+	pass
+
+def uploadFile(fileName, filePath):
+	''' Upload a file using EXP API.  Return UUID '''
+	log_response(response_data)
+
+def getFileData(fileUUID):
+	'''Use the UUID returned from file upload to retrieve file version information'''
+	pass
+
+def deleteFile(fileUUID):
+	'''use the UUID returned from file upload to delete the file in preparation for next test'''
+	pass
+
+if __name__ == '__main__':
+	'''
+	Init code for test - includes logging and configuration file setup
+	'''
 	config = configparser.ConfigParser()
 	config.read(CONFIG_FILE_PATH)
-
-def login
+	baseurl = config['login']['baseurl']
+	username = config['login']['username']
+	password = config['login']['password']
+	logging.config.fileConfig(LOG_CONFIG_FILE_PATH)
+	logging.info('Test is now beginning.  Configuration section complete')
+	session = requests.Session();
